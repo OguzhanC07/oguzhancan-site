d2e0bc87783b7d31a44f815d829883360c90d38a
Oguzhan Can <oguzhan.can@omerd.com>
2025-07-12 17:35:07 +0300

    classic site were added. icon added.

---
+++ src/OguzhanCan.Website/Controllers/ClassicController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OguzhanCan.Website.Controllers
+{
+    public class ClassicController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
